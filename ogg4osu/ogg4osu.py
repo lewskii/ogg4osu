6cba45438443fb4d2c09469f456bd556e4d557ba
lewski <leevia00@gmail.com>
2023-09-26 01:55:56 +0300

    added a name and description to the arg parser

--- ogg4osu/ogg4osu.py
+++ ogg4osu/ogg4osu.py
@@ -2,11 +2,16 @@ from . import audio
 from argparse import ArgumentParser
 
 def main():
-    parser = ArgumentParser()
+    parser = ArgumentParser(
+        prog = "ogg4osu",
+        description = "Converts audio to OGG Vorbis for the purposes of osu!"
+    )
+
     parser.add_argument(
         "input",
         help = "the name of the file you want to convert"
     )
+
     parser.add_argument(
         "output",
         help = "the name of the converted file"
