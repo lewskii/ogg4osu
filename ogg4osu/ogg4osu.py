c4f8bbc429aecc8a574b96ab3544e68812be415e
lewski <leevia00@gmail.com>
2023-09-27 02:12:53 +0300

    made the destination argument actually optional

--- ogg4osu/ogg4osu.py
+++ ogg4osu/ogg4osu.py
@@ -2,6 +2,7 @@
 from . import audio
 from argparse import ArgumentParser
 from ffmpeg import Error as ffmpegError
+from pathlib import Path
 
 def main():
     """Run ogg4osu with arguments from the command line."""
@@ -9,23 +10,26 @@ def main():
         prog = "ogg4osu",
         description = "Converts audio to OGG Vorbis for the purposes of osu!"
     )
-
     parser.add_argument(
         "source",
         help = "the path to the file you want to convert"
     )
-
     parser.add_argument(
         "destination",
         nargs = '?',
         default = None,
         help = "the path the converted file will have (optional)"
     )
-
     args = parser.parse_args()
 
+    source = Path(args.source)
+    if not args.destination:
+        destination = source.with_suffix(".ogg")
+    else:
+        destination = Path(args.destination)
+
     try:
-        audio.convert(args.source, args.destination)
+        audio.convert(source, destination)
     except ffmpegError as error:
         stderr = error.stderr.decode().strip()
         print("stderr:\n", stderr, sep='')
