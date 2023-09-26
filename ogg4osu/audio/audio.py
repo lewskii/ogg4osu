7a82d6fd8024cc04701c9e391990eab18b887c39
lewski <leevia00@gmail.com>
2023-09-26 12:36:49 +0300

    improved documentation

--- ogg4osu/audio/audio.py
+++ ogg4osu/audio/audio.py
@@ -10,12 +10,19 @@ MAX_BITRATE = 208_000
 __BASE_DELAY = 200
 
 
-def convert(infile: PathLike, outfile: PathLike):
-    """Convert an audio file to OGG Vorbis."""
+def convert(infile: PathLike, outfile: PathLike) -> None:
+    """
+    Convert an audio file to OGG Vorbis.
 
+    Arguments:
+
+    infile: the path to the file that will be converted
+
+    outfile: the path where the result of the conversion will be
+    """
     input_audio = (
         ffmpeg
-        .input(infile)
+        .input(str(infile))
         .audio
         .filter('adelay', delays = __BASE_DELAY, all = 1) # delay all channels
     )
@@ -23,7 +30,7 @@ def convert(infile: PathLike, outfile: PathLike):
     audio_info = AudioInfo(infile)
     
     output_audio = input_audio.output(
-        outfile,
+        str(outfile),
         acodec = 'libvorbis',
         aq = 6,
         ar = __reasonable_sample_rate(audio_info.sample_rate)
@@ -34,14 +41,20 @@ def convert(infile: PathLike, outfile: PathLike):
 
 def __reasonable_sample_rate(original_sample_rate: int) -> int:
     """
-    Choose a reasonable sample rate based on a given original one.
+    Return a reasonable sample rate to convert to based on a given original one.
 
-    Here, \"reasonable\" is meant in terms of file size, so any input of 48 kHz
+    Arguments:
+
+    original_sample_rate: the sample rate of the original audio file in hertz.
+
+    Here, \"reasonable\" is meant in terms of file size; any input of 48 kHz
     or less will be returned as is. A higher input will be reduced to either 48
     kHz or 41.1 kHz based on which one it is a multiple of, because integer
     ratios are apparently ideal for resampling.
-    """
 
+    Non-standard inputs are normalised to 48 kHz if they are over the limit and
+    returned as-is otherwise.
+    """
     if original_sample_rate <= 48_000:
         return original_sample_rate
     
--- ogg4osu/audio/audio_info.py
+++ ogg4osu/audio/audio_info.py
@@ -4,22 +4,47 @@ from pathlib import Path
 
 
 class AudioInfo:
-    """Information about an audio file."""
+    """
+    Select information about an audio file, meant for use in conversion.
+    
+    Instance variables:
+
+    codec: the codec the audio was encoded with
+
+    sample_rate: the sample rate of the audio in hertz
+
+    bit_rate: the bit rate of the file in bits per second
+
+    duration: the duration of the audio in seconds
+
+    size: the size of the file in bytes
+
+    path: the path to the file the the information concerns
+
+    json: the raw JSON output from ffprobe
+    """
+
     def __init__(self, file: PathLike) -> None:
         self.path = Path(file).resolve()
+        """The path to the file the the information concerns."""
 
-        probe_data = ffmpeg.probe(file)
-        audio_stream = _find_audio_stream(probe_data["streams"])
-        format = probe_data["format"]
+        self.json = ffmpeg.probe(file)
+        """The raw JSON output from ffprobe."""
+
+        audio_stream = _find_audio_stream(self.json["streams"])
+        format = self.json["format"]
 
         self.codec = audio_stream["codec_name"]
-        self.duration = float(audio_stream["duration"])
         self.sample_rate = int(audio_stream["sample_rate"])
         self.bit_rate = int(format["bit_rate"])
+        self.duration = float(audio_stream["duration"])
         self.size = int(format["size"])
         
 
 def _find_audio_stream(streams: list[dict]) -> dict:
+    """
+    Find a dict that represents an audio stream from a list and return it.
+    """
     for stream in streams:
         if stream["codec_type"] == "audio":
             return stream
