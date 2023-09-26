acf19a4d739327e0c9c428cc772ebe49b8b2926c
lewski <leevia00@gmail.com>
2023-09-26 10:57:33 +0300

    gave AudioInfo's properties sensible data types

--- ogg4osu/audio/audio_info.py
+++ ogg4osu/audio/audio_info.py
@@ -9,17 +9,17 @@ class AudioInfo:
         self.path = Path(file)
 
         probe_data = ffmpeg.probe(file)
-        audio_stream = __find_audio_stream(probe_data["streams"])
+        audio_stream = _find_audio_stream(probe_data["streams"])
         format = probe_data["format"]
 
         self.codec = audio_stream["codec_name"]
-        self.duration = audio_stream["duration"]
-        self.sample_rate = audio_stream["sample_rate"]
-        self.bit_rate = format["bit_rate"]
-        self.size = format["size"]
+        self.duration = float(audio_stream["duration"])
+        self.sample_rate = int(audio_stream["sample_rate"])
+        self.bit_rate = int(format["bit_rate"])
+        self.size = int(format["size"])
         
 
-def __find_audio_stream(streams: list[dict]) -> dict:
+def _find_audio_stream(streams: list[dict]) -> dict:
     for stream in streams:
         if stream["codec_type"] == "audio":
             return stream
